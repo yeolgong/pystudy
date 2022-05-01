9d1c0caea57c4f37202e76b53fc03b49b91cebd4
LeeSY99 <101381138+LeeSY99@users.noreply.github.com>
2022-05-01 21:11:24 +0900

    Update 201902520 5.2.py
    history생성, forward의 오류수정

--- 5회차/201902520 5.2.py
+++ 5회차/201902520 5.2.py
@@ -1,4 +1,4 @@
-
+import sys
 class DNode:
     def __init__(self,e):
         self.data=e
@@ -8,6 +8,8 @@ class DNode:
 class web:
     def __init__(self):
         first=DNode('www.hufs.ac.kr')
+        self.historylist=['www.hufs.ac.kr']
+        self.historyout=[]
         self.head=first
         self.tail=first
         self.now=first
@@ -28,11 +30,14 @@ class web:
             new.back=self.tail
             self.tail=new
             self.now=new
+            self.historylist.append(self.now.data)
             print(self.now.data)
         else:
             new.back=self.now
             self.now.next=new
             self.now=new
+            self.tail=new
+            self.historylist.append(self.now.data)
             print(self.now.data)
         
         
@@ -51,7 +56,35 @@ class web:
             print(self.now.data)
         
     def history(self):
+        self.historyout=[]
+        index=len(self.historylist)-1
+        while index >= 0:
+            if self.historylist[index] not in self.historyout:
+                self.historyout.append(self.historylist[index])
+            index-=1
+        
+        for i in range(len(self.historyout)):
+            print(self.historyout[i])
     
     def quit(self):
         exit()    
+
+explore=web()        
+while True:
+    come=sys.stdin.readline().strip()
+    if come[0:2]=='go':
+        fun,address=come.split()
+    else:
+        fun=come
+     
+    if fun=='go':
+        explore.go(address)
+    elif fun=='forward':
+        explore.forward()
+    elif fun=='backward':
+        explore.backward()
+    elif fun=='history':
+        explore.history()
+    elif fun=='quit':
+        explore.quit()
     
